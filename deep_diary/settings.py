60642fefb17bd7596798eec3d744b4de6c641cd5
Deependra Neupane <deependran.neupane@gmail.com>
2020-05-19 22:22:05 +0545

    'hello'

--- deep_diary/settings.py
+++ deep_diary/settings.py
@@ -132,7 +132,7 @@ db_from_env= dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,"OpenPowerEngineering/static"),
+    os.path.join(BASE_DIR,"deep_diary/static"),
 ]
 STATIC_URL = '/static/'
 
