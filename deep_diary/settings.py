f4f7e5ea25ea7f333a125c4a0e87718c4fc1d47d
Deependra Neupane <deependran.neupane@gmail.com>
2020-05-19 22:09:49 +0545

    'hello'

--- deep_diary/settings.py
+++ deep_diary/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&0er(t#&rcq714(8huxq15l=wobe#muvrq%wnzu=qpb==$d^r)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['deepdiary.herokuapp.com']
 
 
 # Application definition
